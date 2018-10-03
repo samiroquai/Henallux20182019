4c53cd73c5b5c23d30a564a15bab4ab7397f16f8
Samuel Scholtes <samuel.scholtes@outlook.com>
2018-10-03 21:02:49 +0200

    ajout ressources labo ETL

---
+++ BI/LaboETL/DW.DDL.sql
@@ -0,0 +1,261 @@
+USE [MA_DB]
+GO
+CREATE TABLE [dbo].[DimCurrency](
+	[CurrencyKey] [int] IDENTITY(1,1) NOT NULL,
+	[CurrencyAlternateKey] [nchar](3) NOT NULL,
+	[CurrencyName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_DimCurrency_CurrencyKey] PRIMARY KEY CLUSTERED 
+(
+	[CurrencyKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[DimCustomer]    Script Date: 11-10-17 15:43:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DimCustomer](
+	[CustomerKey] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerAlternateKey] [nvarchar](15) NOT NULL,
+	[Title] [nvarchar](8) NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[MiddleName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[NameStyle] [bit] NULL,
+	[BirthDate] [date] NULL,
+	[MaritalStatus] [nchar](1) NULL,
+	[Suffix] [nvarchar](10) NULL,
+	[Gender] [nvarchar](1) NULL,
+	[EmailAddress] [nvarchar](50) NULL,
+	[YearlyIncome] [money] NULL,
+	[TotalChildren] [tinyint] NULL,
+	[NumberChildrenAtHome] [tinyint] NULL,
+	[EnglishEducation] [nvarchar](40) NULL,
+	[SpanishEducation] [nvarchar](40) NULL,
+	[FrenchEducation] [nvarchar](40) NULL,
+	[EnglishOccupation] [nvarchar](100) NULL,
+	[SpanishOccupation] [nvarchar](100) NULL,
+	[FrenchOccupation] [nvarchar](100) NULL,
+	[HouseOwnerFlag] [nchar](1) NULL,
+	[NumberCarsOwned] [tinyint] NULL,
+	[AddressLine1] [nvarchar](120) NULL,
+	[AddressLine2] [nvarchar](120) NULL,
+	[Phone] [nvarchar](20) NULL,
+	[DateFirstPurchase] [date] NULL,
+	[CommuteDistance] [nvarchar](15) NULL,
+	[City] [nvarchar](255) NULL,
+	[PostalCode] [nvarchar](255) NULL,
+	[EnglishCountryRegionName] [nvarchar](255) NULL,
+	[SpanishCountryRegionName] [nvarchar](255) NULL,
+	[FrenchCountryRegionName] [nvarchar](255) NULL,
+ CONSTRAINT [PK_DimCustomer_CustomerKey] PRIMARY KEY CLUSTERED 
+(
+	[CustomerKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[DimDate]    Script Date: 11-10-17 15:43:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DimDate](
+	[DateKey] [int] NOT NULL,
+	[FullDateAlternateKey] [date] NOT NULL,
+	[DayNumberOfWeek] [tinyint] NOT NULL,
+	[EnglishDayNameOfWeek] [nvarchar](10) NOT NULL,
+	[SpanishDayNameOfWeek] [nvarchar](10) NOT NULL,
+	[FrenchDayNameOfWeek] [nvarchar](10) NOT NULL,
+	[DayNumberOfMonth] [tinyint] NOT NULL,
+	[DayNumberOfYear] [smallint] NOT NULL,
+	[WeekNumberOfYear] [tinyint] NOT NULL,
+	[EnglishMonthName] [nvarchar](10) NOT NULL,
+	[SpanishMonthName] [nvarchar](10) NOT NULL,
+	[FrenchMonthName] [nvarchar](10) NOT NULL,
+	[MonthNumberOfYear] [tinyint] NOT NULL,
+	[CalendarQuarter] [tinyint] NOT NULL,
+	[CalendarYear] [smallint] NOT NULL,
+	[CalendarSemester] [tinyint] NOT NULL,
+	[FiscalQuarter] [tinyint] NOT NULL,
+	[FiscalYear] [smallint] NOT NULL,
+	[FiscalSemester] [tinyint] NOT NULL,
+ CONSTRAINT [PK_DimDate_DateKey] PRIMARY KEY CLUSTERED 
+(
+	[DateKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[DimProduct]    Script Date: 11-10-17 15:43:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[DimProduct](
+	[ProductKey] [int] IDENTITY(1,1) NOT NULL,
+	[ProductAlternateKey] [nvarchar](25) NULL,
+	[WeightUnitMeasureCode] [nchar](3) NULL,
+	[SizeUnitMeasureCode] [nchar](3) NULL,
+	[EnglishProductName] [nvarchar](50) NOT NULL,
+	[SpanishProductName] [nvarchar](50) NOT NULL,
+	[FrenchProductName] [nvarchar](50) NOT NULL,
+	[StandardCost] [money] NULL,
+	[FinishedGoodsFlag] [bit] NOT NULL,
+	[Color] [nvarchar](15) NOT NULL,
+	[SafetyStockLevel] [smallint] NULL,
+	[ReorderPoint] [smallint] NULL,
+	[ListPrice] [money] NULL,
+	[Size] [nvarchar](50) NULL,
+	[SizeRange] [nvarchar](50) NULL,
+	[Weight] [float] NULL,
+	[DaysToManufacture] [int] NULL,
+	[ProductLine] [nchar](2) NULL,
+	[DealerPrice] [money] NULL,
+	[Class] [nchar](2) NULL,
+	[Style] [nchar](2) NULL,
+	[ModelName] [nvarchar](50) NULL,
+	[LargePhoto] [varbinary](max) NULL,
+	[EnglishDescription] [nvarchar](400) NULL,
+	[FrenchDescription] [nvarchar](400) NULL,
+	[ChineseDescription] [nvarchar](400) NULL,
+	[ArabicDescription] [nvarchar](400) NULL,
+	[HebrewDescription] [nvarchar](400) NULL,
+	[ThaiDescription] [nvarchar](400) NULL,
+	[GermanDescription] [nvarchar](400) NULL,
+	[JapaneseDescription] [nvarchar](400) NULL,
+	[TurkishDescription] [nvarchar](400) NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+	[Status] [nvarchar](7) NULL,
+	[SubCategoryEnglishName] [nvarchar](255) NOT NULL,
+	[SubCategorySpanishName] [nvarchar](255) NOT NULL,
+	[SubCategoryFrenchName] [nvarchar](255) NOT NULL,
+	[CategoryEnglishName] [nvarchar](255) NOT NULL,
+	[CategorySpanishName] [nvarchar](255) NOT NULL,
+	[CategoryFrenchName] [nvarchar](255) NOT NULL,
+ CONSTRAINT [PK_DimProduct_ProductKey] PRIMARY KEY CLUSTERED 
+(
+	[ProductKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_DimProduct_ProductAlternateKey_StartDate] UNIQUE NONCLUSTERED 
+(
+	[ProductAlternateKey] ASC,
+	[StartDate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[DimPromotion]    Script Date: 11-10-17 15:43:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DimPromotion](
+	[PromotionKey] [int] IDENTITY(1,1) NOT NULL,
+	[PromotionAlternateKey] [int] NULL,
+	[EnglishPromotionName] [nvarchar](255) NULL,
+	[SpanishPromotionName] [nvarchar](255) NULL,
+	[FrenchPromotionName] [nvarchar](255) NULL,
+	[DiscountPct] [float] NULL,
+	[EnglishPromotionType] [nvarchar](50) NULL,
+	[SpanishPromotionType] [nvarchar](50) NULL,
+	[FrenchPromotionType] [nvarchar](50) NULL,
+	[EnglishPromotionCategory] [nvarchar](50) NULL,
+	[SpanishPromotionCategory] [nvarchar](50) NULL,
+	[FrenchPromotionCategory] [nvarchar](50) NULL,
+	[StartDate] [datetime] NOT NULL,
+	[EndDate] [datetime] NULL,
+	[MinQty] [int] NULL,
+	[MaxQty] [int] NULL,
+ CONSTRAINT [PK_DimPromotion_PromotionKey] PRIMARY KEY CLUSTERED 
+(
+	[PromotionKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_DimPromotion_PromotionAlternateKey] UNIQUE NONCLUSTERED 
+(
+	[PromotionAlternateKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[FactInternetSales]    Script Date: 11-10-17 15:43:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FactInternetSales](
+	[ProductKey] [int] NOT NULL,
+	[OrderDateKey] [int] NOT NULL,
+	[DueDateKey] [int] NOT NULL,
+	[ShipDateKey] [int] NOT NULL,
+	[CustomerKey] [int] NOT NULL,
+	[PromotionKey] [int] NOT NULL,
+	[CurrencyKey] [int] NOT NULL,
+	[SalesOrderNumber] [nvarchar](20) NOT NULL,
+	[SalesOrderLineNumber] [tinyint] NOT NULL,
+	[RevisionNumber] [tinyint] NOT NULL,
+	[OrderQuantity] [smallint] NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[ExtendedAmount] [money] NOT NULL,
+	[UnitPriceDiscountPct] [float] NOT NULL,
+	[DiscountAmount] [float] NOT NULL,
+	[ProductStandardCost] [money] NOT NULL,
+	[TotalProductCost] [money] NOT NULL,
+	[SalesAmount] [money] NOT NULL,
+	[TaxAmt] [money] NOT NULL,
+	[Freight] [money] NOT NULL,
+	[CarrierTrackingNumber] [nvarchar](25) NULL,
+	[CustomerPONumber] [nvarchar](25) NULL,
+	[OrderDate] [datetime] NULL,
+	[DueDate] [datetime] NULL,
+	[ShipDate] [datetime] NULL,
+ CONSTRAINT [PK_FactInternetSales_SalesOrderNumber_SalesOrderLineNumber] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderNumber] ASC,
+	[SalesOrderLineNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimCurrency] FOREIGN KEY([CurrencyKey])
+REFERENCES [dbo].[DimCurrency] ([CurrencyKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimCurrency]
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimCustomer] FOREIGN KEY([CustomerKey])
+REFERENCES [dbo].[DimCustomer] ([CustomerKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimCustomer]
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimDate] FOREIGN KEY([OrderDateKey])
+REFERENCES [dbo].[DimDate] ([DateKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimDate]
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimDate1] FOREIGN KEY([DueDateKey])
+REFERENCES [dbo].[DimDate] ([DateKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimDate1]
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimDate2] FOREIGN KEY([ShipDateKey])
+REFERENCES [dbo].[DimDate] ([DateKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimDate2]
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimProduct] FOREIGN KEY([ProductKey])
+REFERENCES [dbo].[DimProduct] ([ProductKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimProduct]
+GO
+ALTER TABLE [dbo].[FactInternetSales]  WITH CHECK ADD  CONSTRAINT [FK_FactInternetSales_DimPromotion] FOREIGN KEY([PromotionKey])
+REFERENCES [dbo].[DimPromotion] ([PromotionKey])
+GO
+ALTER TABLE [dbo].[FactInternetSales] CHECK CONSTRAINT [FK_FactInternetSales_DimPromotion]
+GO
